99f65f0e5824cc5ea1b51d547fa0de1ea33b380e
DSCLANDESTINE <m.davoodibusinessacc@gmail.com>
2025-04-12 23:58:29 +0330

    added the rgb color picker feature...

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,11 +1,14 @@
 import DraggableDrawer from "../Components/DraggableDrawer";
+import RGBChart from "../Components/RGBChart"
 
 import Dropzone from "../Components/DropZone";
 export default function MainPage() {
   return (
     <div className="w-11/12 h-10/12 mx-auto">
+
       <DraggableDrawer />
       <div className="flex flex-col pt-10 w-full h-full mx-auto">
+        <RGBChart/>
         <Dropzone />
       </div>
     </div>
