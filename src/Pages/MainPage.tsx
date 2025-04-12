3a33e19fbe36b29b910bb85167bc4a2129b88b3f
DSCLANDESTINE <m.davoodibusinessacc@gmail.com>
2025-04-12 23:49:28 +0330

    linked the components togethere

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,11 +1,13 @@
 import DraggableDrawer from "../Components/DraggableDrawer";
 
 import Dropzone from "../Components/DropZone";
+import WaioTech from "../Components/WaioTech"
 export default function MainPage() {
   return (
     <div className="w-11/12 h-10/12 mx-auto">
       <DraggableDrawer />
       <div className="flex flex-col pt-10 w-full h-full mx-auto">
+        <WaioTech/>
         <Dropzone />
       </div>
     </div>
